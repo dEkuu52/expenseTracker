7f4ad9436c01b95bac59d8314cb590df007d55c1
friks <friksgon694@gmail.com>
2026-07-21 19:56:39 +0300

    minor code changes for correct operation, moving class-based code to a separate file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import database
+import models
 import matplotlib
 from prettytable import from_db_cursor
 from datetime import datetime
@@ -9,7 +9,7 @@ from datetime import datetime
 def main_func():
     welcome_message()
 
-    choice = input('Enter the number of the function you need: ')
+    choice = input('Enter the number of the function you need(If you want to exit, enter "Exit"): ')
 
     if choice == '1':
         create_new_acc()
@@ -21,6 +21,8 @@ def main_func():
         add_trans()
     elif choice =='6':
         delete_acc()
+    elif choice.strip().lower() == 'Exit':
+        exit()
 
 
 # ----------- Welcome func -----------
@@ -30,7 +32,7 @@ def welcome_message():
             content = file.read()
             print(content)
     except FileNotFoundError:
-        print('File not found')
+        print('❌ File not found')
 
 
 # ------------ 1. Create acc func ------------
@@ -38,21 +40,26 @@ def create_new_acc():
     all_entries = {}
 
     while True:
-        name = input('Enter your account name: ')
+        name = input('Enter your account name(If you want to exit, enter "Exit"): ')
         try:
             balance = float(input('Enter your account balance: '))
         except ValueError:
             print('Please enter a valid number')
             continue
 
-        print('Your account has been created.')
+        print('☑️ Your account has been created.')
 
-        new_entry = database.AddInData(account_name=name, balance=balance)
+        new_entry = models.AddInData(account_name=name, balance=balance)
         new_entry.save_to_db()
 
         all_entries[name, balance] = new_entry
-        break
+
+        if name or balance == 'Exit':
+            print("🕗 Returning to main menu...")
+            main_func()
+            break
     return all_entries
+
 
 # ------------ 2. Add category func ------------
 def add_category():
@@ -60,28 +67,30 @@ def add_category():
 
     while True:
         # get_acc
-        db_cursor_acc = database.GetInAcc().get_all_acc()
+        db_cursor_acc = models.GetInAcc().get_all_acc()
         print(from_db_cursor(db_cursor_acc))
 
         account_id = input('Enter your account id(or "Exit"): ')
         if account_id.strip().lower() == 'exit':
-            print("Returning to main menu...")
+            print("🕗 Returning to main menu...")
             main_func()
+            break
 
         category_name = input('Enter your category name: ')
 
         if category_name.strip().lower() == 'exit':
-            print("Returning to main menu...")
+            print("🕗 Returning to main menu...")
             main_func()
+            break
 
         type_category = input('Enter your category type (Indicate here whether this is "income" or an "expense"): ')
 
         if type_category.strip().lower() == 'exit':
-            print("Returning to main menu...")
+            print("🕗 Returning to main menu...")
             main_func()
         print('Once you have added all the categories you need, you can enter "Exit" to return to the main menu.')
 
-        new_entry = database.AddInCategory(
+        new_entry = models.AddInCategory(
             account_id=int(account_id),
             category_name=category_name,
             type=type_category,
@@ -91,9 +100,22 @@ def add_category():
 
 # ------------ 3. Get acc func ------------
 def get_acc():
-    db_cursor = database.GetInAcc().get_all_acc()
+    while True:
+        db_cursor = models.GetInAcc().get_all_acc()
+        print(from_db_cursor(db_cursor))
+        db_cursor_cat = models.GetInCategory().get_all_category()
+        print(from_db_cursor(db_cursor_cat))
+
+        choice = input('If you want to exit, enter "Exit": ')
+        if choice.strip().lower() == 'Exit':
+            print("🕗 Returning to main menu...")
+            main_func()
+
+
+def get_acc_for_del():
+    db_cursor = models.GetInAcc().get_all_acc()
     print(from_db_cursor(db_cursor))
-    db_cursor_cat = database.GetInCategory().get_all_category()
+    db_cursor_cat = models.GetInCategory().get_all_category()
     print(from_db_cursor(db_cursor_cat))
 
 # ------------ 4. Transactions ------------
@@ -107,21 +129,26 @@ def add_trans():
     if not date:
         date = datetime.today().strftime('%Y-%m-%d')
 
-    tx_obg = database.Transaction(account_id, category_id, amount, description, date)
+    tx_obg = models.Transaction(account_id, category_id, amount, description, date)
     tx_obg.add_transaction()
 
 
 
 # ------------ 6. Delete acc ------------
 def delete_acc():
-    get_acc()
-    selected_acc = input('Enter the id of the account you want to delete: ')
+    while True:
+        get_acc_for_del()
+        selected_acc = input('Enter the id of the account you want to delete(If you want to exit, enter "Exit"): ')
 
-    id_delete_db = int(selected_acc)
+        id_delete_db = int(selected_acc)
 
-    deleter = database.DeleteInData(account_id=id_delete_db, category_id=None)
-    deleter.delete_from_db()
-    print('Account has been deleted.')
+        deleter = models.DeleteInData(account_id=id_delete_db, category_id=None)
+        deleter.delete_from_db()
+        print('☑️ Account has been deleted.')
+
+        if  selected_acc or id_delete_db == 'Exit':
+            print("🕗 Returning to main menu...")
+            main_func()
 
 
 if __name__ == "__main__":
---
+++ models.py
@@ -0,0 +1,120 @@
+import sqlite3
+from database import cursor, connection
+
+
+class AddInData:
+    def __init__(self, account_name, balance):
+        self.account_name = account_name
+        self.balance = balance
+
+    def save_to_db(self):
+        cursor.execute(
+            "INSERT INTO accounts(account_name, balance) VALUES(?, ?)",
+            (self.account_name, self.balance)
+        )
+        connection.commit()
+
+class GetInAcc(AddInData):
+    def __init__(self):
+        super().__init__(account_name=None, balance=None)
+    def get_all_acc(self):
+        cursor.execute('SELECT * FROM accounts')
+        return cursor
+    connection.commit()
+
+class AddInCategory:
+    def __init__(self, account_id, category_name, type):
+        self.account_id = account_id
+        self.category_name = category_name
+        self.type = type
+    def save_to_db(self):
+        cursor.execute(
+            "INSERT INTO categories(id_account,name, type) VALUES(?,?,?)",(self.account_id,self.category_name, self.type)
+        )
+        connection.commit()
+
+class GetInCategory(AddInCategory):
+    def __init__(self):
+        super().__init__(account_id=None,category_name=None, type=None)
+    def get_categories_id_account(self):
+        cursor.execute('SELECT id,name, type FROM categories WHERE id_account=?',(self.account_id,))
+        return cursor
+    def get_all_category(self):
+        cursor.execute('SELECT * FROM categories')
+        return cursor
+    connection.commit()
+
+class Transaction:
+    def __init__(self, account_id, category_id, amount, description, date):
+        self.account_id = account_id
+        self.category_id = category_id
+        self.amount = amount
+        self.description = description
+        self.date = date
+    def add_transaction(self):
+        cursor.execute(
+            'SELECT type FROM categories WHERE id = ?',
+            (self.category_id,))
+
+        category_type = cursor.fetchone()[0]
+
+        cursor.execute(
+            'INSERT INTO transactions(account_id, category_id, amount, description, date) VALUES(?,?,?,?,?)',
+            (self.account_id, self.category_id, self.amount, self.description, self.date)
+        )
+
+        if category_type == 'income':
+            cursor.execute('''
+                UPDATE accounts 
+                SET balance = balance + ?
+                WHERE id = ?
+                                 ''',(self.amount, self.account_id)
+            )
+        elif category_type == 'expense':
+            cursor.execute('''
+            UPDATE accounts 
+            SET balance = balance - ?
+            WHERE id = ?    
+            ''', (self.amount,self.account_id)
+            )
+        connection.commit()
+        print('☑️The transaction was successful.')
+    def search_transaction(self):
+        cursor.execute('''
+            SELECT
+                transactions.amount,
+                transactions.description,
+                transactions.date,
+                accounts.account_name,
+                categories.name AS category_name
+            FROM transactions
+            JOIN accounts ON transactions.account_id = accounts.id
+            JOIN categories ON transactions.category_id = categories.id
+            WHERE accounts.id = ? AND transactions.category_id = ?
+        ''', (self.account_id, self.category_id))
+
+
+class DeleteInData:
+    def __init__(self, account_id, category_id):
+        self.account_id = account_id
+        self.category_id = category_id
+    def delete_from_db(self):
+        if self.account_id is not None:
+            cursor.execute(
+                "UPDATE transactions SET account_id = NULL WHERE account_id = ?",
+                (self.account_id,)
+            )
+            cursor.execute(
+                "DELETE FROM categories WHERE id_account = ?",
+                (self.account_id,)
+            )
+            cursor.execute(
+                "DELETE FROM accounts WHERE id = ?",
+                (self.account_id,)
+            )
+        elif self.category_id is not None:
+            cursor.execute(
+                "DELETE FROM categories WHERE id = ?",
+                (self.category_id,)
+            )
+        connection.commit()
